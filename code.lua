e2d2ad50ecabb712b61b411a6f85226cc865efa4
yvy1989 <andersonyvy1@gmail.com>
2022-04-07 21:57:01 -0400

    Modificacao de desenho d sprites
    Foram modificados as formas de desenho dos sprites e criados metadados de 4 sprites no diretorio Sprites

--- code.lua
+++ code.lua
@@ -6,66 +6,20 @@
  
 local message = "EMPTY LUA \n\n\nThis is an empty  template.\n\n\nVisit 'www.pixelvision8.com' to learn more about creating games from scratch."
 
-Player1_Positions = {
-  player_up = {
-    sprite1 = 2,
-    sprite2 = 3,
-    sprite3 = 18,
-    sprite4 = 19
-  },
-  player_down = {
-    sprite1 = 8,
-    sprite2 = 9,
-    sprite3 = 24,
-    sprite4 = 25
-  },
-  player_left = {
-    sprite1 = 4,
-    sprite2 = 5,
-    sprite3 = 20,
-    sprite4 = 21
-  },
-  player_right = {
-    sprite1 = 12,
-    sprite2 = 13,
-    sprite3 = 28,
-    sprite4 = 29
-  }
-}
 
-player_up = {
-  sprite1 = 2,
-  sprite2 = 3,
-  sprite3 = 18,
-  sprite4 = 19
-}
+
 
 
 Player1_Orientation = "up" -- variavel que guarda a direcao do player1
 
+local px = 67 
+local py = 69 
 
 
-local px1 = 67 
-local py1 = 69 
-local sprite1 = 2 
-
-local px2 = 74
-local py2 = 69
-local sprite2 = 3
-
-local px3 = 67
-local py3 = 76
-local sprite3 = 18
-
-local px4 = 74
-local py4 = 76
-local sprite4 = 19
-
-
-local dx = 0;
-local dy = 0;
-local friccao = 0.8
-local aceleracao = 0.4
+local Player1_dx = 0;
+local Player1_dy = 0;
+local Player1_friccao = 0.8
+local Player1_aceleracao = 0.4
 
 
 
@@ -87,83 +41,47 @@ end
 
 function Draw() -- redesenha
 
+  DrawText("Flag " .. Flag(px, py), 10, 50, DrawMode.Sprite, "large", 15)
 
   RedrawDisplay()--apaga a tela e redesenha o tilemap
   
-  if Player1_Orientation == "down"
-  then 
-    DrawSprite(Player1_Positions.player_down.sprite1,px1,py1,false,false,DrawMode.Sprite)
-    DrawSprite(Player1_Positions.player_down.sprite2,px2,py2,false,false,DrawMode.Sprite)
-    DrawSprite(Player1_Positions.player_down.sprite3,px3,py3,false,false,DrawMode.Sprite)
-    DrawSprite(Player1_Positions.player_down.sprite4,px4,py4,false,false,DrawMode.Sprite)  
-  end
+  DrawMetaSprite("tank_" .. Player1_Orientation,px,py,false,false,DrawMode.Sprite)
 
-  if Player1_Orientation == "up"
-  then 
-    DrawSprite(Player1_Positions.player_up.sprite1,px1,py1,false,false,DrawMode.Sprite)
-    DrawSprite(Player1_Positions.player_up.sprite2,px2,py2,false,false,DrawMode.Sprite)
-    DrawSprite(Player1_Positions.player_up.sprite3,px3,py3,false,false,DrawMode.Sprite)
-    DrawSprite(Player1_Positions.player_up.sprite4,px4,py4,false,false,DrawMode.Sprite)  
-  end
-
-  if Player1_Orientation == "left"
-  then 
-    DrawSprite(Player1_Positions.player_left.sprite1,px1,py1,false,false,DrawMode.Sprite)
-    DrawSprite(Player1_Positions.player_left.sprite2,px2,py2,false,false,DrawMode.Sprite)
-    DrawSprite(Player1_Positions.player_left.sprite3,px3,py3,false,false,DrawMode.Sprite)
-    DrawSprite(Player1_Positions.player_left.sprite4,px4,py4,false,false,DrawMode.Sprite)  
-  end
-
-  if Player1_Orientation == "right"
-  then 
-    DrawSprite(Player1_Positions.player_right.sprite1,px1,py1,false,false,DrawMode.Sprite)
-    DrawSprite(Player1_Positions.player_right.sprite2,px2,py2,false,false,DrawMode.Sprite)
-    DrawSprite(Player1_Positions.player_right.sprite3,px3,py3,false,false,DrawMode.Sprite)
-    DrawSprite(Player1_Positions.player_right.sprite4,px4,py4,false,false,DrawMode.Sprite)  
-  end
-
-  
   
 
 end
 
 
 function control_check()
-  dx *=friccao
-  dy *=friccao
+  Player1_dx *=Player1_friccao
+  Player1_dy *=Player1_friccao
 
   if Button(Buttons.Right) then 
-    dx += aceleracao 
+    Player1_dx += Player1_aceleracao 
     Player1_Orientation = "right"
   end
 
 
   if Button(Buttons.Left) then 
-    dx -= aceleracao 
+    Player1_dx -= Player1_aceleracao 
     Player1_Orientation = "left"
   end
 
 
   if Button(Buttons.Up) then 
-    dy -= aceleracao 
+    Player1_dy -= Player1_aceleracao 
     Player1_Orientation = "up"
   end
 
 
   if Button(Buttons.Down) 
-  then dy += aceleracao 
+  then Player1_dy += Player1_aceleracao 
     Player1_Orientation = "down"
   end
 
-  px1 += dx
-  px2 += dx
-  px3 += dx
-  px4 += dx
+  px += Player1_dx
+  py += Player1_dy
 
-  py1 += dy
-  py2 += dy
-  py3 += dy
-  py4 += dy
 
 end
 
